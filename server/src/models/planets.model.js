77c31c8f75300e04e928ea7df5dd2b59d1cb4a57
Kevsely <elegis.sossou@yahoo.com>
2022-03-31 05:40:24 +0100

    Setting up the planets.model

--- server/src/models/planets.model.js
+++ server/src/models/planets.model.js
@@ -1,3 +1,40 @@
-const planets = []
+const fs = require('fs')
+const path = require("path")
+const { parse } = require('csv-parse');
 
-module.exports = planets
+const parser = parse({
+    comment: "#",
+    columns: true
+});
+
+const habitablePlanets = [];
+
+function isHabitablePlanets(planet) {
+    return planet["koi_disposition"] === "CONFIRMED"
+        && planet["koi_insol"] > 0.36 && planet["koi_insol"] < 1.11
+        && planet["koi_prad"] < 1.6
+}
+
+function loadPlanetsData () {
+    return new Promise((resolve, reject) => {
+        fs.createReadStream(path.join(__dirname, "..", "..", "data", "kepler_data.csv"))
+            .pipe(parser)
+            .on("data", (data) => {
+                if (isHabitablePlanets(data))
+                    habitablePlanets.push(data);
+            })
+            .on("error", (error) => {
+                console.log(error)
+                reject(error)
+            })
+            .on("end", () => {
+                console.log(`${habitablePlanets.length} habitables planets`)
+                resolve()
+            })
+    })    
+}
+
+module.exports = {
+    loadPlanetsData,
+    planets: habitablePlanets
+}

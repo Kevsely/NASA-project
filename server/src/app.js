4f995ff2aba8b2120be2fc5d2042717ffe98f7e6
Kevsely <elegis.sossou@yahoo.com>
2022-11-29 17:21:51 +0100

    ...

--- server/src/app.js
+++ server/src/app.js
@@ -1,8 +1,14 @@
 const express = require('express')
+const cors = require('cors')
 
 const planetsRouter = require('./routers/planets/planets.router')
 
 const app = express()
+
+app.use(cors({
+    origin: 'http://localhost:3000'
+}))
+
 app.use(express.json())
 app.use('/planets', planetsRouter)
 

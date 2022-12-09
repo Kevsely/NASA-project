a1d3b20ed57550707e5a4cf77012d7fe81b8276c
Kevsely <elegis.sossou@yahoo.com>
2022-12-09 18:57:31 +0100

    Added feature - Save the list of habitablables planets directly into the mongodb

--- server/src/models/planets.model.js
+++ server/src/models/planets.model.js
@@ -4,8 +4,6 @@ const path = require('path')
 
 const planets = require('./planets.mongo')
 
-const habitablePlanets = []
-
 function isHabitablePlanet(planet) {
 	return (
 		planet['koi_disposition'] === 'CONFIRMED' &&
@@ -16,38 +14,59 @@ function isHabitablePlanet(planet) {
 }
 
 function loadPlanetsData() {
-    return new Promise((resolve, reject) => {
-        fs.createReadStream(path.join(__dirname, '..', '..', 'data', 'kepler_data.csv'))
-            .pipe(
-                parse({
-                    comment: '#',
-                    columns: true,
-                })
-            )
-            .on('data', async (data) => {
-                if (isHabitablePlanet(data)) {
-                    // TODO: Replace below create with insert + update = upsert
-                    // await planets.create({
-                    //     keplerName: data.kepler_name
-                    // })
-                }
-            })
-            .on('error', (err) => {
-                console.log(err)
-                reject(err)
-            })
-            .on('end', () => {
-                console.log('Done')
-                resolve()
-            })
-    })
+	return new Promise((resolve, reject) => {
+		fs.createReadStream(
+			path.join(__dirname, '..', '..', 'data', 'kepler_data.csv')
+		)
+			.pipe(
+				parse({
+					comment: '#',
+					columns: true,
+				})
+			)
+			.on('data', async (data) => {
+				if (isHabitablePlanet(data)) {
+                    savePlanet(data)
+				}
+			})
+			.on('error', (err) => {
+				console.log(err)
+				reject(err)
+			})
+			.on('end', async () => {
+                const countPlanetsFound = (await getAllPlanets()).length
+                console.log(
+					`Found ${countPlanetsFound} habitables planets`
+				)
+				console.log('Done')
+				resolve()
+			})
+	})
 }
 
 async function getAllPlanets() {
-    return await planets.find({})
+	return await planets.find({})
+}
+
+async function savePlanet(planet) {
+	try {
+		await planets.updateOne(
+			{
+				keplerName: planet.kepler_name,
+			},
+			{
+				keplerName: planet.kepler_name,
+			},
+			{
+				upsert: true,
+			}
+		)
+	} catch (err) {
+		console.error(`Could not found save the planet ${err}`)
+	}
 }
 
 module.exports = {
-    loadPlanetsData,
-    getAllPlanets,
+	loadPlanetsData,
+	getAllPlanets,
 }
